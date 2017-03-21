f07d7b65a761d09047928ceaf0d9080a2bd3c485
RohanBh <brohan52@gmail.com>
2017-03-22 03:51:44 +0530

    Alternative to initial bar code scanner that I added that also scans qr codes also

--- app/src/main/java/com/sdsmdg/cognizance2017/activities/BarCodeActivity.java
+++ app/src/main/java/com/sdsmdg/cognizance2017/activities/BarCodeActivity.java
@@ -2,6 +2,7 @@ package com.sdsmdg.cognizance2017.activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.PointF;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,13 +11,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+import com.sdsmdg.cognizance2017.R;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+public class BarCodeActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
 
-public class BarCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-
-    private static final int MY_PERMISSIONS_REQUEST_CAM = 1;
+    /*private static final int MY_PERMISSIONS_REQUEST_CAM = 1;
     private ZXingScannerView mScannerView;
     private String barCodeResult;
 
@@ -85,6 +85,89 @@ public class BarCodeActivity extends AppCompatActivity implements ZXingScannerVi
 
         // If you would like to resume scanning, call this method below:
         mScannerView.resumeCameraPreview(this);
+    }*/
+    private QRCodeReaderView mydecoderview;
+    private String barCodeResult;
+    private static final int MY_PERMISSIONS_REQUEST_CAM = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            int permissionCheck = ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.CAMERA);
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAM);
+            } else {
+                //Permission is granted
+            }
+        } else {
+            //Permission available
+        }
+
+        // Use this function to enable/disable decoding
+        mydecoderview.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        //mydecoderview.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        //mydecoderview.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        //mydecoderview.setFrontCamera();
+
+        // Use this function to set back camera preview
+        mydecoderview.setBackCamera();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAM: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+
+                } else {
+                    // permission denied
+                    finish();
+                }
+                return;
+            }
+        }
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed in View
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        // Do something with the result here
+        barCodeResult = text; //stores text in barcoderesult
+        Toast.makeText(this, barCodeResult, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.stopCamera();
     }
 
 }
